e06c9780bb6671575ba39c687af92cdcdc71baf9
Kaan Türkmen <kaanturkmen@protonmail.com>
2022-04-17 18:53:14 +0300

    Initial commit.

---
+++ src/main/java/tr/edu/ku/devnull/kudos/KudosApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.ku.devnull.kudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/ku/devnull/kudos/KudosApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.ku.devnull.kudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
